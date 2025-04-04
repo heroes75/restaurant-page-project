743702d3451395655e70432b5a85c474f6ce38a0
heroes75 <123456789+odin@users.noreply.github.com>
2025-04-04 23:11:13 +0000

    finish the homepage in html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import  backgroundImages from "./images/13312339_v755-toon-05.jpg"; 
 import "./styles.css";
-console.log("sdasdad")
+
+ 
+console.log("sdasdad");
+document.body.style.backgroundImage = `url(${backgroundImages})`;
