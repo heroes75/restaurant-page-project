a67ceb7b3a83359f798a3d8cb030538312d928ed
heroes75 <123456789+odin@users.noreply.github.com>
2025-04-07 18:24:48 +0000

    almost finish to write a home page

--- src/homePage.js
+++ src/homePage.js
@@ -6,7 +6,12 @@ export const writingHomepage = () => {
     const tittleContainer = document.createElement("div");
     const img1 = document.createElement("img");
     const img2 = document.createElement("img");
-    const img3 = document.createElement("img")
+    const img3 = document.createElement("img");
+    const presentationContainer = document.createElement("div");
+    const tittlePresentation = document.createElement("h3");
+    const textPresentation = document.createElement("div");
+    const locationContainer = document.createElement("div");
+    const locationText = document.createElement("div");
     img1.src = cremeGlacee1bg;
     img1.style.width = "70px";
     img1.style.height = "70px";
@@ -19,13 +24,25 @@ export const writingHomepage = () => {
     const h1tittle = document.createElement("h1")
     tittleContainer.setAttribute("id", "tittle-container");
     frameHome.setAttribute("id", "frame-home");
-    h1tittle.textContent = "Ice Cream Shop";
+    presentationContainer.setAttribute("id", "presentation-container");
+    tittlePresentation.setAttribute("id", "tittle-presentation");
+    textPresentation.setAttribute("id", "text-presentation");
+    locationContainer.setAttribute("id", "location-container");
+    locationText.setAttribute("id", "location-text");
+    h1tittle.textContent = "Jisoo's Ice Cream Shop";
+    tittlePresentation.textContent = "About Jisoo's Ice Cream Shop";
+    locationText.innerHTML = "<span>Location</span><span>good location</span>";
+    textPresentation.innerHTML = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus sagittis luctus. Integer pharetra dui ut turpis mollis consequat. Vivamus sed mauris facilisis, blandit nulla ut, venenatis tellus.<p> Suspendisse et pharetra odio. Vivamus quam ante, tincidunt ut venenatis eget, laoreet in lectus. In varius accumsan convallis. Aliquam vehicula, ex non semper vehicula, neque ex facilisis augue, quis commodo diam urna ac odio. </p><p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas commodo justo vel sapien tristique congue.</p>";
     document.querySelector("#content").appendChild(frameHome);
     frameHome.appendChild(tittleContainer);
     tittleContainer.appendChild(img1);
     tittleContainer.appendChild(h1tittle);
     tittleContainer.appendChild(img2);
     frameHome.appendChild(img3);
-
+    frameHome.appendChild(presentationContainer);
+    frameHome.appendChild(locationContainer);
+    presentationContainer.appendChild(tittlePresentation);
+    presentationContainer.appendChild(textPresentation);
+    locationContainer.appendChild(locationText);
 
 }
